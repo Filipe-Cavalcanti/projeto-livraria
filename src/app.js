9a163ab2ec7687c3c8aec98284f850475a30435f
Luis Filipe Santos <luiscsantos92@gmail.com>
2026-09-25 12:50:01 -0300

    Integra o Express à API (versão 4.18  para acompanhar o estudo)

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 import express from "express";
 
 const app = express();
+app.use(express.json()); // Middleware
+
+const livros = [
+    {
+        id: 1,
+        titulo: "O Senhor dos Anéis"
+    },
+    {
+        id:2,
+        titulo: "O Hobbit"
+    }
+]
 
 app.get("/", (req, res) => {
-    res.status(200).send("Curso de node.js");
+    res.status(200).send("Curso de node.js"); //.send envia mensagens simples apenas
 });
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros); // para envio de dados mais elaborados, utilizar .json
+});
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso");
+})
 
 export default app;
